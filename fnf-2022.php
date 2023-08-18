6d244e6f4e9e9cbbba1bfaa069d873fb95806680
jyothiranjan25 <jyothiranjan25@gmail.com>
2023-08-18 20:23:44 +0530

    small fix

--- fnf-2022.php
+++ fnf-2022.php
@@ -143,37 +143,7 @@
   </section>
   <!-- /about -->
   <!-- Highlights -->
-  <section class="fnp22 bg-gray">
-    <h2 class="text-center">Milestones of FNF 2022</h2>
-    <div class="container mt-5 mb-0">
-      <div class="row text-center">
-        <div class="col">
-          <div class="counter1"> <i class="fa fa-graduation-cap fa-5x" aria-hidden="true"></i>
-            <h2 class="timer count-title1 count-number" data-to="230" data-speed="7000">₹</h2>
-            <p class="count-text ">Students</p>
-          </div>
-        </div>
-        <div class="col">
-          <div class="counter1"> <i class="fa fa-globe fa-5x" aria-hidden="true"></i>
-            <h2 class="timer count-title1 count-number" data-to="10" data-speed="7000"></h2>
-            <p class="count-text ">Countries</p>
-          </div>
-        </div>
-        <div class="col">
-          <div class="counter1"> <i class="fa fa-university fa-5x" aria-hidden="true"></i>
-            <h2 class="timer count-title1 count-number" data-to="162" data-speed="7000"></h2>
-            <p class="count-text ">Schools</p>
-          </div>
-        </div>
-        <div class="col">
-          <div class="counter1"> <i class="fa fa-smile-o fa-5x" aria-hidden="true"></i>
-            <h2 class="timer count-title1 count-number" data-to="230" data-speed="7000"></h2>
-            <p class="count-text ">Happy Participants</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <?php include_once "milestones.php"; ?>
   <!-- /Highlights -->
 
   <!-- /footer -->
--- header.php
+++ header.php
@@ -12,8 +12,8 @@
 				<button class="navbar-toggler rounded-0" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 				<div class="collapse navbar-collapse" id="navigation">
 					<ul class="navbar-nav ml-auto text-center">
-						<li class="nav-item active"> <a class="nav-link" href="index.php">FNF 2023</a> </li>
-						<li class="nav-item @@about"> <a class="nav-link" href="leadership-retreat.php">Leadership Retreat</a> </li>
+						<li class="nav-item"> <a class="nav-link" href="index.php">FNF 2023</a> </li>
+						<li class="nav-item"> <a class="nav-link" href="leadership-retreat.php">Leadership Retreat</a> </li>
 						<li class="nav-item"> <a class="nav-link" href="fnf-2022.php">FNF 2022</a> </li>
 						<!-- <li class="nav-item @@register  "> <a class="nav-link" href="register_closed.php?source=<?= $source ?>&medium=<?= $medium ?>&campaign=<?= $campaign ?>" target="_blank">FNF 2022</a> </li> -->
 						<li class="nav-item @@brochure  dropdown"> <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Relevant Documents</a>
